d73715d9a1868cbbe17f0fce9ec31b0292505c96
user1 <user1@DESKTOP-EHF0CJD>
2024-11-15 11:51:17 +0900

    [ADD] 24/11/15 변수와 자료형 그리고 상수
    1. 변수는 변하는 수이다. 리터럴을 저장하고
    이름표의 역학을 한다.
    2.자료형은 리터럴에

---
+++ src/Exam_Constants.java
@@ -0,0 +1,19 @@
+
+
+public class Exam_Constants {
+
+	public static void main(String[] args) {
+		// 상수는 변하지 않는 수
+		//선언시 대문자로 표시
+		final double PI = 3.14;
+		
+		
+		//원의 면적 공식 : 3.14 * r * r
+		double radius = 5.7;
+		double circleArea;
+		circleArea = PI * radius * radius;
+		System.out.println("원의면적 :" + circleArea);
+
+	}
+
+}
---
+++ src/Exam_Variable.java
@@ -0,0 +1,21 @@
+
+
+public class Exam_Variable {
+
+	public static void main(String[] args) {
+		//변수의 선언
+		int num;
+		//변수의 초기화
+		num = 1118;
+		// int num = 1118;
+		String str = "Hello Wolrd";
+		System.out.println(1118);
+		System.out.println(num);
+		System.out.println(str);
+		num = 515;
+		System.out.println(num);
+		
+
+	}
+
+}
